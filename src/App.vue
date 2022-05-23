62b0d68dc24331026cce9db7942adddad799df9a
Jiří Kofroň <jiri.kofron@signi.com>
2022-05-23 23:54:34 +0200

    initial commit with almost clean Vue3 project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import HomePage from "@/components/HomePage.vue";
+</script>
+
+<template>
+  <HomePage />
+</template>
+
+<style>
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+</style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Hello from Home page</h1>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+};
+</script>
+
+<style scoped></style>
